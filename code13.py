36c717f65d16a9f39be0118d8d492e3ec856bab3
renegadelha <renegadelha@gmail.com>
2023-02-13 20:19:05 -0300

    aula1202

---
+++ code13.py
@@ -0,0 +1,12 @@
+#estrutura de decisão
+   #simples
+   #encadeadas
+
+idade = int(input('digite sua idade'))
+if(idade >= 18):
+    tempo = idade - 18
+    print('pode vender bebida')
+    print(f'ele(a) ja pode comprar bebida ha {tempo} ano(s)')
+else:
+    tempo = 18 - idade
+    print(f'NAO pode vender bebida e deve esperar {tempo} ano(s)')
